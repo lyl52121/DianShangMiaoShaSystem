558f821d7702c9778e1eecb638e15cf19672008c
LiYuanli <2601756464@qq.com>
2019-04-19 16:37:41 +0800

    应用SpringBoot完成基础项目搭建

--- workspaces/src/main/java/com/cn/lyl/miaosha/App.java
+++ workspaces/src/main/java/com/cn/lyl/miaosha/App.java
@@ -1,7 +1,12 @@
 package com.cn.lyl.miaosha;
 
+import com.cn.lyl.miaosha.dao.UserDOMapper;
+import com.cn.lyl.miaosha.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
  * Hello world!
  *
  */
-@EnableAutoConfiguration//自动化配置
+//@EnableAutoConfiguration//自动化配置
+@SpringBootApplication(scanBasePackages = {"com.cn.lyl.miaosha"})
 @RestController
+@MapperScan("com.cn.lyl.miaosha.dao")
 public class App 
 {
+    @Autowired
+    private UserDOMapper userDOMapper;
+
     @RequestMapping("/")
     public String home(){
-        return "hello,lixiang3!";
+        UserDO userDO=userDOMapper.selectByPrimaryKey(1);
+        if(userDO==null)
+        {
+            return "用户对象不存在";
+        }else {
+            return userDO.getName();
+        }
+//        return "hello,lixiang3!";
     }
     public static void main( String[] args )
     {
